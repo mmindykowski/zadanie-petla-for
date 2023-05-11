9046f2d706e234f5a2fcf513d80d6588a324285c
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-05-11 21:47:34 +0200

    cwiczenie

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const cities = ["Szczecin", "Bydgoszcz", "Poznań", "Zielona Góra", "Gdańsk"];
+
+for (let i = 0; i < cities.length; i++) {
+  console.log(`To miasto nazywa się: ${cities[i].toUpperCase()}`);
+}
